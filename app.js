1832f620578ba544d5b92bba46158e8eb34cfcce
cfpbj <cfpbj251@icloud.com>
2025-11-16 22:35:06 +0900

    ファイルwo

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
-app.use('/notes', notesRouterotesRouter);
+app.use('/notes', notesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
